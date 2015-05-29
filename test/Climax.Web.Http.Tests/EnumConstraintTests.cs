1e2f027e1de1150174f1fa400c69df2bfb58e1e5
filipw <wojcieszyn@gmail.com>
2015-05-29 17:11:20 +0200

    fixed tests

--- test/Climax.Web.Http.Tests/EnumConstraintTests.cs
+++ test/Climax.Web.Http.Tests/EnumConstraintTests.cs
@@ -7,13 +7,18 @@ using Should;
 
 namespace Climax.Web.Http.Tests
 {
+    public enum TestEnum
+    {
+        Foo
+    }
+
     [TestFixture]
     public class EnumConstraintTests
     {
         [Test]
         public void ShouldMatch_IfParameterValue_InTheEnumMembers([Values("foo", "notfoo")] string input)
         {
-            var constraint = new EnumConstraint("Honda.Models.Web.Tests.TestEnum, Honda.Models.Web.Tests");
+            var constraint = new EnumConstraint("Climax.Web.Http.Tests.TestEnum, Climax.Web.Http.Tests");
 
             var result = constraint.Match(new HttpRequestMessage(), new Mock<IHttpRoute>().Object, "test",
                 new HttpRouteValueDictionary
